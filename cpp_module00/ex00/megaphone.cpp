a7c7c61ec2831f52c31ee92209fb991de7fb5bab
myukang <myunghwan0421@gmail.com>
2022-07-04 13:43:21 +0900

    FEAT:study

--- cpp_module00/ex00/megaphone.cpp
+++ cpp_module00/ex00/megaphone.cpp
@@ -1,17 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MegaPhone.cpp                                      :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: myukang <myukang@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 21:46:33 by myukang           #+#    #+#             */
-/*   Updated: 2022/07/01 21:49:28 by myukang          ###   ########.fr       */
+/*   Updated: 2022/07/03 17:27:36 by myukang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <ostream>
+
+class megaphone
+{
+	private:
+		int ac;
+		char **argv;
+	public:
+		megaphone(const int ac, const char **argv) : ac(ac), argv(argv)
+};
 
 int	main(int ac, char **argv)
 {
+	megaphone	phone(ac, argv);
 
-
+	return (0);
 }
---
+++ study/BasicsofC++/first.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<cout<<cout<<endl;
+	cout<<"hello world!\n";
+	return 0;
+}
